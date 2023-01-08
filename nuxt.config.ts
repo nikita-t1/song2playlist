da666933bf862fa5b2e096d566f4e9c87b2011e6
nikita-t1 <26936278+nikita-t1@users.noreply.github.com>
2023-01-08 10:43:48 +0100

    Add VueUse

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: ['nuxt-icon'],
+    modules: ['nuxt-icon', '@vueuse/nuxt',],
     css: ['~/assets/css/main.css'],
     postcss: {
         plugins: {
