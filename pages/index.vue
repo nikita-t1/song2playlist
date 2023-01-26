04813bb273f0d8867bf6b99616029b5e709934a8
nikita-t1 <26936278+nikita-t1@users.noreply.github.com>
2023-01-26 19:30:32 +0100

    Create index.vue

---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="flex flex-row h-screen">
+
+        <div class="w-72 min-w-[18rem] bg-black flex flex-col justify-end p-4 border-1 border border-spotify-green ">
+            <span class="text-white font-bold">Queue:</span>
+            <Queue class="overflow-hidden overflow-y-scroll my-4"/>
+            <Player/>
+            <SpotifyTokenValidity class="text-center"/>
+        </div>
+
+        <Playlists />
+
+
+    </div>
+
+
+</template>
+
+<script lang="ts" setup>
+import Queue from "~/components/Queue.vue";
+import Player from "~/components/Player.vue";
+import SpotifyTokenValidity from "~/components/SpotifyTokenValidity.vue";
+import Playlists from "~/components/Playlists.vue";
+</script>
+
+<style scoped>
+
+</style>
