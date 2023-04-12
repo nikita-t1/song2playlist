f11b3914d249b342a0314988780454122bad7e7f
nikita-t1 <26936278+nikita-t1@users.noreply.github.com>
2023-04-12 23:00:42 +0200

    feat: add favicon

--- app.vue
+++ app.vue
@@ -1,6 +1,23 @@
 <template>
-  <div>
-      <NuxtPage :key="$route.fullPath"/>
-  </div>
+	<div>
+		<NuxtPage :key="$route.fullPath"/>
+	</div>
 </template>
+
+<script lang="ts">
+const isDark = usePreferredDark()
+const favicon = computed(() => isDark.value ? 'song2playlist_light.ico' : 'song2playlist_dark.ico')
+
+export default defineNuxtComponent({
+    head(nuxtApp: any) {
+        return {
+            title: 'song2playlist',
+            link: [
+                {rel: 'icon', type: 'image/x-icon', href: "/" +  favicon.value},
+            ]
+        }
+    },
+})
+</script>
+
 
