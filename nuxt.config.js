19d71cd809a0b51a9f9afc46bd8c849ca6161816
nikita-t1 <26936278+nikita-t1@users.noreply.github.com>
2023-06-05 21:38:27 +0200

    build: add pinia to package.json

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: ['nuxt-icon', '@vueuse/nuxt',],
+    modules: ['nuxt-icon', '@vueuse/nuxt', '@pinia/nuxt',],
     css: ['~/assets/css/main.css'],
     postcss: {
         plugins: {
