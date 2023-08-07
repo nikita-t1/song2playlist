9254d338db42befaf717bb1f82aa871bcc3898b1
nikita-t1 <26936278+nikita-t1@users.noreply.github.com>
2023-08-07 19:54:15 +0200

    update: darker green border if song name matched, but not song id

--- components/PlaylistCard.vue
+++ components/PlaylistCard.vue
@@ -22,11 +22,22 @@ const image = computed(() => {
     return props.playlist.images[0]?.url ?? ""
 })
 
+enum TrackMatch {
+    ID,
+    NAME,
+    NONE
+}
+
 const isCurrentTrackInPlaylist = computed(() => {
     const pbs = playbackState.value
-    if (!pbs) return false
+    if (!pbs) return TrackMatch.NONE
     // compare the current track id with the playlist tracks
-    return props.playlist?.allTracks?.some((it: PlaylistTrackObject) => it.track?.id == pbs.item!.id) ?? false
+    const matchedTrackId = props.playlist?.allTracks?.some((it: PlaylistTrackObject) => it.track?.id == pbs.item!.id) ?? false
+    if (matchedTrackId) return TrackMatch.ID
+    // compare the current track name with the playlist tracks
+    const matchedTrackName = props.playlist?.allTracks?.some((it: PlaylistTrackObject) => it.track?.name == pbs.item!.name) ?? false
+    if (matchedTrackName) return TrackMatch.NAME
+    return TrackMatch.NONE
 })
 
 onMounted(() => {
@@ -54,16 +65,17 @@ const hover = ref(false)
 
 <template>
     <div class="group">
-        <div :class="isCurrentTrackInPlaylist ? 'bg-emerald-500 hover:bg-red-500' : 'bg-blue-500'"
-             class="cursor-pointer w-8 h-8 absolute -m-2 text-center rounded-lg rounded-br-lg text-white duration-500"
-             @mouseleave="hover = false" @mouseover="hover = true">
-            <Icon v-if="isCurrentTrackInPlaylist" :name="hover ? 'bi:x' : 'bi:check-lg'" size="24"
+        <div
+            :class="{ 'bg-emerald-500 hover:bg-red-500': isCurrentTrackInPlaylist != TrackMatch.NONE, 'bg-blue-500': isCurrentTrackInPlaylist == TrackMatch.NONE, 'bg-spotify-green-darkest' : isCurrentTrackInPlaylist == TrackMatch.NAME}"
+            class="cursor-pointer w-8 h-8 absolute -m-2 text-center rounded-lg rounded-br-lg text-white duration-500"
+            @mouseleave="hover = false" @mouseover="hover = true">
+            <Icon v-if="isCurrentTrackInPlaylist != TrackMatch.NONE" :name="hover ? 'bi:x' : 'bi:check-lg'" size="24"
                   @click="removeTrackToPlaylist"/>
             <Icon v-else name="bi:plus" size="24" @click="addTrackToPlaylist"/>
         </div>
 
         <img v-if="playlist"
-             :class="{'outline outline-4 outline-spotify-green outline-offset-4': isCurrentTrackInPlaylist}"
+             :class="{'outline outline-4 outline-offset-4': isCurrentTrackInPlaylist != TrackMatch.NONE, 'outline-spotify-green': isCurrentTrackInPlaylist == TrackMatch.ID, 'outline-spotify-green-darkest' : isCurrentTrackInPlaylist == TrackMatch.NAME}"
              :src="image" alt="Playlist Image"
              class="h-48 w-48 rounded duration-300 group-hover:outline-none">
         <div class="mt-1 text-center text-white">{{ playlist.name }}</div>
