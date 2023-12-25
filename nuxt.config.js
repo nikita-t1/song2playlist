0ce44eb1b9485babcae0b2cd7e5468a10f6d9186
nikita-t1 <26936278+nikita-t1@users.noreply.github.com>
2023-12-25 21:22:23 +0100

    build(npm): add primevue to package.json

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,12 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     ssr: false,
-    modules: ['nuxt-icon', '@vueuse/nuxt', '@pinia/nuxt',],
+    modules: ['nuxt-icon', '@vueuse/nuxt', '@pinia/nuxt', 'nuxt-primevue'],
     css: ['~/assets/css/main.css'],
     postcss: {
         plugins: {
             tailwindcss: {},
             autoprefixer: {},
         },
+    },
+    primevue: {
+        unstyled: true
     },
 })
